dc2e5a64590afa5049098312282c675d96fd5674
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2024-04-14 18:29:17 -0300

    Update dashcode.py

--- dashcode.py
+++ dashcode.py
@@ -25,4 +25,51 @@ def create_scatter_chart(x_axis="alcohol", y_axis="malic_acid", color_encode=Fal
 
 def create_bar_chart(ingredients=["alchool", "malic_acid", "ash"]):
     bar_fig = px.bar(avg_wine_df, x="WineType", y=ingredients, title="Avg. Ingrecients per Wine Type")
+    bar_fig.update_layout(height=600)
+    return bar_fig
+
+# Widgets
+x_axis = dcc.Dropdown(id="x_axis", options=ingredients, value="alcohol", clearable=False, style={"display": "inline-block", "width": "48%"})
+y_axis = dcc.Dropdown(id="y_axis", options=ingredients, value="malic_acid", clearable=False, style={"display": "inline-block", "width": "48%"})
+color_encode = dcc.Checklist(id="color_encode", options=["Color-Encode",])
+
+multi_select = dcc.Dropdown(id="multi_select", options=ingredients, value=["alcohol", "malic_acid", "ash"], clearable=False, multi=True)
+
+## Web App Layout
+app = Dash(title="Wine Analysis")
+
+app.layout = html.Div(
+    children=[
+        html.H1("Wine Dataset Analysis", style={"text-align": "center"}),
+        html.Div("Explore relationship between various ingredients used in creation of three different types of Wines (class_0, class_1 & class_2)", style={"text-align": "center"}),
+        html.Br(),
+        html.Div(
+            children=[
+                    x_axis, y_axis, color_encode,
+                    dcc.Graph(id="scatter_chart", figure=create_scatter_chart())
+            ],
+            style={"display": "inline-block", "width": "48%"}
+        ),
+        html.Div(
+            children=[
+                    multi_select, html.Br(),
+                    dcc.Graph(id="bar_chart", figure=create_bar_chart())
+            ],
+            style={"display": "inline-block", "width": "48%"}
+        )
+    ],
+    style={"padding": "50px"}
+)
+
+## Callbacks
+@callback(Output("scatter_chart", "figure"), [Input("x_axis", "value"), Input("y_axis", "value"), Input("color_encode", "value")])
+def update_scatter_chart(x_axis, y_axis, color_encode):
+    return create_scatter_chart(x_axis, y_axis, color_encode)
+
+@callback(Output("bar_chart", "figure"), [Input("multi_select", "value"), ])
+def update_bar_chart(ingredients):
+    return create_bar_chart(ingredients)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
     
